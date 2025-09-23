4ced0c78d01b918ae7716f45f9272debdaf1c6fc
Angel Solano <ad.solano@icloud.com>
2025-09-23 14:47:35 -0600

    Se agregaron diseños en .css

---
+++ IMG/Convertidor de metros a kilómetros_files/codigo.js
@@ -0,0 +1,27 @@
+function converirMetrosKilometros() {
+    //Una variable es un espacio en memoria que guarda un valor
+    metros = parseInt(document.getElementById("cajametros").value);
+    //Operación
+    Kilometros = metros / 1000;
+    //Carga la variable del valor en la caja kilometros de la página.
+    document.getElementById("cajakilometros").value = Kilometros;
+}
+
+function ObtenerIMC() {
+    //Una variable es un espacio en memoria que guarda un valor
+    peso = parseInt(document.getElementById("cajapeso").value);
+    altura = parseFloat(document.getElementById("cajaaltura").value);
+    //Operación
+    IMC = peso / (altura * altura);
+    //Carga la variable del valor en la caja kilometros de la página.
+    document.getElementById("cajaimc").value = IMC;
+}
+
+function ConvertirCelsius() {
+    //Una variable es un espacio en memoria que guarda un valor
+    Celsius = parseInt(document.getElementById("cajacelsius").value);
+    //Operación
+    Fahrenheit = 9/5 * Celsius + 32;
+    //Carga la variable del valor en la caja kilometros de la página.
+    document.getElementById("cajafahrenheit").value = Fahrenheit;
+}
